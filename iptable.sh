ffa0d8f1b3435c3b82a3eae9a9aff172b00bbd30
Shadiqq <shadiqazkayusuf@gmail.com>
2026-02-07 19:20:19 +0700

    Enhance iptable.sh for IP forwarding and NAT
    Updated iptable.sh to include IP forwarding and NAT configuration.

--- iptable.sh
+++ iptable.sh
@@ -1,3 +1,21 @@
-sudo sysctl -w net.ipv4.ip_forward=1
+#!/bin/bash
 
-sudo iptables -R POSTROUTING 1 -t nat -j MASQUERADE -o enp4s0 -s 10.45.0.0/16
+# Interface internet (ubah sesuai punyamu, misal eth0 / wlan0 / enp0s3)
+IFACE="eth0"
+
+echo "Enable IP Forwarding..."
+sysctl -w net.ipv4.ip_forward=1
+
+echo "Flush iptables lama..."
+iptables -F
+iptables -t nat -F
+
+echo "Set NAT internet sharing..."
+iptables -t nat -A POSTROUTING -o $IFACE -j MASQUERADE
+
+echo "Allow forwarding trafik UE..."
+iptables -A FORWARD -i ogstun -o $IFACE -j ACCEPT
+iptables -A FORWARD -i $IFACE -o ogstun -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+echo "Done. Iptables ready."
+
